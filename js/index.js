e0c15ec21d69dbb93548373dfd3940bd9c88ed2b
Md Neamul Hoqe <hoqe1997@gmail.com>
2023-08-22 10:48:39 +0600

    updated validations

--- js/index.js
+++ js/index.js
@@ -2,13 +2,16 @@ console.clear();
 
 function getValue(elementId) {
   const Field = document.getElementById(elementId);
-  const String = Field.value;
+  const String = Field.value || 0;
   const fieldValue = parseFloat(String);
-  if (fieldValue < 0) {
+  if (isNaN(Field.value)) {
+    alert("Please, give number input.");
     Field.focus();
-    return alert(
-      "Please, give correct informations about: " + elementId + " money. "
-    );
+    return;
+  } else if (fieldValue < 0) {
+    Field.focus();
+    alert("Please, give correct information about: " + elementId + " money. ");
+    return null;
   }
   return fieldValue;
 }
@@ -26,25 +29,26 @@ budgetCalcBtn.addEventListener("click", function () {
   const RentCost = getValue("Rent");
   const ClothesCost = getValue("Clothes");
 
-  if (
-    income === undefined ||
-    foodCost === undefined ||
-    RentCost === undefined ||
-    ClothesCost === undefined
-  ) {
-    return false;
+  console.log(income, foodCost, RentCost, ClothesCost);
+  if (income) {
+    const totalExpenses = foodCost + RentCost + ClothesCost;
+    checkNan(totalExpenses, totalExpensesElementText);
+
+    const balance = income - totalExpenses;
+    if (balance > 0) balanceElementText.innerText = balance;
+    else
+      balanceElementText.innerHTML = `<p class='text-red-500'>Please, fill information fields correctly.</p>`;
   }
-  const totalExpenses = foodCost + RentCost + ClothesCost;
-  totalExpensesElementText.innerText = totalExpenses;
-
-  const balance = income - totalExpenses;
-  balanceElementText.innerText = balance;
-
   saveBtn.addEventListener("click", function () {
     const savePercent = getValue("save");
     const save = income * (savePercent / 100);
-
-    savingAmountElementText.innerText = save;
-    remainingBalanceElementText.innerText = balance - save;
+    checkNan(save, savingAmountElementText);
+    checkNan(balance - save, remainingBalanceElementText);
   });
 });
+
+function checkNan(value, field) {
+  if (isNaN(value))
+    field.innerHTML = `<p class='text-red-500'>Please, fill information fields correctly.</p>`;
+  else field.innerText = value;
+}
